e0b80db26081a4686f21d0b054ff1514da9e43ce
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-02-06 15:18:14 +0400

    start- nav bar

---
+++ SocialNetwork/social-network/src/Components/Header/HeaderNav/HeaderNav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from  './HeaderNav.module.css';
+
+const HeaderNav = () => {
+	return (
+		<div className={styles.topbar}>
+			<p>Тут наш код</p>
+		</div>
+	);
+}
+
+export default HeaderNav;
