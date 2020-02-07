694c99782aacf3567153d42c94c5dd8942e43cf2
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-02-07 17:35:22 +0400

    +block-main

--- Portfolio/portfolio/src/Components/SectionMain/Main.jsx
+++ Portfolio/portfolio/src/Components/SectionMain/Main.jsx
@@ -7,6 +7,16 @@ function Main() {
 		<section>
 			<div className='container'>
 				<div className={style.main}>
+					<div className={style.main_wrap}>
+						<div className={style.discrt}>
+							<span> Привет! </span>
+							<span> Меня зовут Петр Иванов </span>
+							<span> Я фронтенд-разработчик </span>
+						</div>
+						<div className={style.main_img}>
+							<img src="" alt=""/>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
