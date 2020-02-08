a1de63364884ffac772ed350cbb3491df7d40cfa
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-02-08 12:25:08 +0400

    +block offline

--- Portfolio/portfolio/src/Components/SectionOffline/Offline.jsx
+++ Portfolio/portfolio/src/Components/SectionOffline/Offline.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import style from './Offline.module.css';
 
 
+
 function Offline() {
 	return (
 		<section>
 			<div className='container'>
 				<div className={style.offline}>
+					<div className={style.wrap}>
+						<div className={style.title}>
+							Рассматриваю варианты удаленной работы
+						</div>
+						<button className={style.button}>Заказать работу</button>
+					</div>
 				</div>
 			</div>
 		</section>
