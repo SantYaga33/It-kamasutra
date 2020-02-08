ed086e640641e5a5c33afc0499836edf76ccb9d6
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-02-08 13:35:07 +0400

    +block footer

--- Portfolio/portfolio/src/Components/Footer/Footer.jsx
+++ Portfolio/portfolio/src/Components/Footer/Footer.jsx
@@ -6,8 +6,20 @@ function Footer() {
 	return (
 		<section>
 			<div className='container'>
-				<div className={style.footer}>
-				</div>
+				<footer className={style.footer}>
+					<div className={style.title}>
+						Иван Иванов
+					</div>
+					<div className={style.icons}>
+						<div className={style.icon_img}><img src="" alt=""/></div>
+						<div className={style.icon_img}><img src="" alt=""/></div>
+						<div className={style.icon_img}><img src="" alt=""/></div>
+						<div className={style.icon_img}><img src="" alt=""/></div>
+					</div>
+					<div className={style.copyrights}>
+						&copy; 2020 все права защищены
+					</div>
+				</footer>
 			</div>
 		</section>
 	);
--- Portfolio/portfolio/src/Components/SectionContacts/Contacts.jsx
+++ Portfolio/portfolio/src/Components/SectionContacts/Contacts.jsx
@@ -7,6 +7,15 @@ function Contacts() {
 		<section>
 			<div className='container'>
 				<div className={style.contacts}>
+					<div className={style.title}>
+						Контакты
+					</div>
+					<form className={style.form} action="">
+						<input/>
+						<input/>
+						<textarea  rows="10" cols="80" name="text" placeholder="Ваше сообщение"></textarea>
+						<button  type="submit" className={style.button}>Отправить</button>
+					</form>
 				</div>
 			</div>
 		</section>
