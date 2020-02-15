fe6ed0c6ea4854fb6b20f67d231941567cf33776
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-02-15 15:28:20 +0400

    Updates

---
+++ precache-manifest.15320602eb4f217e98789a0e4c289733.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19359c98787a8a4f34a2353316c80ef",
+    "url": "/It-kamasutra/SocialNetwork/social-network/index.html"
+  },
+  {
+    "revision": "06aadc1638123dcfbee2",
+    "url": "/It-kamasutra/SocialNetwork/social-network/static/css/main.e11bb671.chunk.css"
+  },
+  {
+    "revision": "90760dae08edf2284cc2",
+    "url": "/It-kamasutra/SocialNetwork/social-network/static/js/2.46788eee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/It-kamasutra/SocialNetwork/social-network/static/js/2.46788eee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06aadc1638123dcfbee2",
+    "url": "/It-kamasutra/SocialNetwork/social-network/static/js/main.aab6ff91.chunk.js"
+  },
+  {
+    "revision": "a354eba290856295229a",
+    "url": "/It-kamasutra/SocialNetwork/social-network/static/js/runtime-main.0abe7f9b.js"
+  },
+  {
+    "revision": "a449b46d487062d5c15c9cfd6b45aa3e",
+    "url": "/It-kamasutra/SocialNetwork/social-network/static/media/follow1.a449b46d.jpg"
+  },
+  {
+    "revision": "3f86f4d61ad61b9a6f17d2a45bbabc8b",
+    "url": "/It-kamasutra/SocialNetwork/social-network/static/media/follow2.3f86f4d6.jpg"
+  },
+  {
+    "revision": "73325b22131d9eacf7981453423637de",
+    "url": "/It-kamasutra/SocialNetwork/social-network/static/media/follow3.73325b22.jpg"
+  },
+  {
+    "revision": "3f86f4d61ad61b9a6f17d2a45bbabc8b",
+    "url": "/It-kamasutra/SocialNetwork/social-network/static/media/follow4.3f86f4d6.jpg"
+  },
+  {
+    "revision": "afe01c29568f91aa1cdb6489886e86af",
+    "url": "/It-kamasutra/SocialNetwork/social-network/static/media/follow5.afe01c29.jpg"
+  },
+  {
+    "revision": "73325b22131d9eacf7981453423637de",
+    "url": "/It-kamasutra/SocialNetwork/social-network/static/media/follow6.73325b22.jpg"
+  },
+  {
+    "revision": "c8ae46f671e6383fb0ac94914af3c4a7",
+    "url": "/It-kamasutra/SocialNetwork/social-network/static/media/main-bg.c8ae46f6.jpg"
+  },
+  {
+    "revision": "3f86f4d61ad61b9a6f17d2a45bbabc8b",
+    "url": "/It-kamasutra/SocialNetwork/social-network/static/media/navAva1.3f86f4d6.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/It-kamasutra/SocialNetwork/social-network/precache-manifest.15320602eb4f217e98789a0e4c289733.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/It-kamasutra/SocialNetwork/social-network/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
