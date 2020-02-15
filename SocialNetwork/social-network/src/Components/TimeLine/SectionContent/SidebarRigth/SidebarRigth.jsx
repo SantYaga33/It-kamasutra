d707fb28ebab31502243fac448605b840321588f
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-02-16 01:01:40 +0400

    +

--- SocialNetwork/social-network/src/Components/TimeLine/SectionContent/SidebarRigth/SidebarRigth.jsx
+++ SocialNetwork/social-network/src/Components/TimeLine/SectionContent/SidebarRigth/SidebarRigth.jsx
@@ -4,12 +4,14 @@ import Socials from "./Socials/Socials";
 import Shortcuts from "./Shortcuts/Shortcuts";
 import Recent from "./Recent/Recent";
 import Followers from "./Followers/Followers";
+import Banner from "./Banner/Banner";
 
 
 const SidebarRigth = () => {
 	return (
 		<div className={styles.sidebar_wrap	}>
 			<div className={styles.sidebar}>
+				<Banner/>
 				<Socials/>
 				<Shortcuts/>
 				<Recent/>
